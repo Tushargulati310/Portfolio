b302fe681f43d2039348877912afd53df86ce711
Tushar Gulati <tushargulati310@gmail.com>
2024-09-19 18:08:02 +0530

    Added Responsiveness

---
+++ script.js
@@ -0,0 +1,44 @@
+//Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('fa-xmark');
+    navBar.classList.toggle('active');
+}
+
+//Scroll section active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll=()=>{
+    sections.forEach(sec =>{
+        let top= window.scrollY;
+        let offset = sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let is = sec.getAttribute('id');
+        if(top >= offset && top< offset + height){
+            navLinks.forEach.apply(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' +id +']').classList.add('active');
+            });
+        };
+    });
+
+    // Sticky Navbar
+    let header= document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY>100);
+
+
+    //Remove toggle icon and navbar
+    menuIcon.classList.remove('fa-xmark');
+    navBar.classList.remove('active');
+}
+
+ScrollReveal({ 
+    distance:'80px',
+    duration:2000,
+    delay:200,
+});
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
